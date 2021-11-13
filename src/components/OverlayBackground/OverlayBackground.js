0b7f28e774ccf4d74ddd62d7fc38f93d2b419815
Zatrimaylova <m.zatrimaylova@gmail.com>
2021-11-13 19:13:22 +0300

    feat: add function to close overlay by click

--- src/components/OverlayBackground/OverlayBackground.js
+++ src/components/OverlayBackground/OverlayBackground.js
@@ -1,14 +1,31 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
+import { ACTION_USE_ADDING } from '../../ducks/adding';
+import { ACTION_USE_REMOVING } from '../../ducks/removing';
+import { ACTION_USE_AMOUNT } from '../../ducks/amount';
+
 import {
   Overlay,
   OverlayBody,
 } from './styles';
 
 class OverlayBackground extends React.Component {
+  confirmDeleting = (e) => {
+    const { adding, removing, amount, showAdding, showRemoving, showAmount } = this.props;
+    if (e.target.id !== 'overlay_background') return;
+
+    if (adding) showAdding({isOpen: false, product: ''});
+    if (removing) showRemoving({isOpen: false, product: ''});
+    if (amount) showAmount({isOpen: false, product: ''});
+
+    document.body.style.overflow = 'visible';
+  }
+
   render() {
     return (
-      <Overlay>
+      <Overlay id="overlay_background" onClick={this.confirmDeleting}>
         <OverlayBody>
           {this.props.children}
         </OverlayBody>
@@ -17,4 +34,17 @@ class OverlayBackground extends React.Component {
   }
 }
 
-export default OverlayBackground;
+const mapStateToProps = ({ adding, removing, amount }) => ({
+  adding: adding.adding.isOpen,
+  removing: removing.removing.isOpen,
+  amount: amount.amount.isOpen,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  showAdding: (value) => dispatch(ACTION_USE_ADDING(value)),
+  showRemoving: (value) => dispatch(ACTION_USE_REMOVING(value)),
+  showAmount: (value) => dispatch(ACTION_USE_AMOUNT(value)),
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(OverlayBackground);
